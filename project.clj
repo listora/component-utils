9dd4718624dcf347cf6d023d4aad22871e4f3312
James Reeves <jreeves@weavejester.com>
2014-10-01 18:23:35 +0100

    Add description and URL to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject listora/component-utils "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Utility function for the Component library"
+  :url "https://github.com/listora/component-utils"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[com.stuartsierra/component "0.2.2"]
